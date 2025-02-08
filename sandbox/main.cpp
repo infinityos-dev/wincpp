d00508501b90aa05a202a52e04ed40b4a9155516
Viktor Popp <viktorpopp.dev@gmail.com>
2025-02-08 15:49:54 +0100

    Added a lot of more features

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -30,12 +30,8 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
     Window window(wndClass.GetClassNameW(), L"Hello", hInstance);
     window.Show(nCmdShow);
-
-    MSG msg = { 0 };
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+    window.SetTitle(L"Hey");
+    window.RunDefaultMessageLoop();
 
     return 0;
 }
